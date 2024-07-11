f112bdbda20f923bab8545305a92c94508303bfb
Diego Martinez <diegomrey1@gmail.com>
2024-07-11 21:34:19 +0200

    create folders, navbar and home page

---
+++ src/containers/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="sticky top-0 p-3 flex justify-center gap-3">
+        <p>Experiencia</p>
+        <p>Proyectos</p>
+        <p>Sobre mi</p>
+        <p>Contacto</p>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/Page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <article className="max-w-4xl bg-slate-500 flex justify-center">
+        <section className="max-w-4xl flex flex-col items-center">
+          <h1>Page</h1>
+        </section>
+        <section className="max-w-4xl flex flex-col items-center">
+          <h1>Page</h1>
+        </section>
+      </article>
+    </>
+  );
+};
+
+export default Page;
