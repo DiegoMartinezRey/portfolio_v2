4c20806b8aadf613becdb57000e35d476acc4b1a
Diego Martinez <diegomrey1@gmail.com>
2024-07-11 23:10:50 +0200

    add data about experiences

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import experiencesData from "../data/experiences.json";
 
 const Content = () => {
   return (
     <>
-      <section className="py-36 max-w-4xl flex flex-col items-center">
-        <h1>Content</h1>
-      </section>
+      <div className="py-36 max-w-4xl flex flex-col items-center">
+        <section className="p-4">
+          <h2 className="m-2">Experiencia</h2>
+          <ul className="flex flex-col gap-3">
+            {experiencesData.map((experience, index) => (
+              <li key={index}>
+                <h3>{experience.name}</h3>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section>Proyectos</section>
+        <section>Sobre mi</section>
+      </div>
     </>
   );
 };
