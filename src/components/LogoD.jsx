b862f1be66f19913da0a8aeea7920112d5e5ff2b
Diego Martinez <diegomrey1@gmail.com>
2024-09-06 12:19:50 +0200

    Changes

---
+++ src/components/LogoD.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const LogoD = () => {
+  return (
+    <div className="flex flex-col justify-center items-center group cursor-pointer">
+      <div className="w-32 transition-all group-hover:-translate-y-2">
+        <img src="/logo/logo_top_light2.png" alt="" />
+      </div>
+      <div className="w-36 absolute ml-2 mb-1.5 opacity-0 transition-all group-hover:opacity-100">
+        <img src="/logo/logo_name_light.png" alt="" />
+      </div>
+      <div className="w-32 transition-all group-hover:translate-y-2">
+        <img src="/logo/logo_button_light2.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default LogoD;
--- src/containers/Profile.jsx
+++ src/containers/Profile.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Button from "../components/Button";
+import LogoD from "../components/LogoD";
 
 const Profile = () => {
   return (
     <>
-      <section className="max-w-4xl flex flex-col items-center py-36 bg-red-400">
+      <section className="max-w-4xl min-h-screen flex flex-col items-center py-36">
         <h1>Hola! soy Diego Martinez</h1>
         <h2>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. A, commodi
@@ -12,6 +13,7 @@ const Profile = () => {
           voluptatibus debitis alias esse fuga.
         </h2>
         <Button title={"LinkedIn"} />
+        <LogoD />
       </section>
     </>
   );
